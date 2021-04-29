d6d0aa550856a8d36161d3a44a2b41dba7770046
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-04-29 13:23:37 -0700

    Completed prep-javascript-functions.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,80 @@
+// addTwoNumbers Exercise
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+var addTwoNumbersResult = addTwoNumbers(2, 2);
+
+console.log('addTwoNumbers Exercise:', addTwoNumbersResult);
+
+// convertHoursToMinutes Exercise
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+var convertHoursToMinutesResult = convertHoursToMinutes(2);
+
+console.log('convertHoursToMinutes Exercise:', convertHoursToMinutesResult);
+
+// getGreeting Exercise
+function getGreeting(name) {
+  return 'Hello ' + name + '!';
+}
+
+var getGreetingResult = getGreeting('World');
+
+console.log('getGreeting Exercise:', getGreetingResult);
+
+// addAndMultiplyBy5 Exercise
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+
+var addAndMultiplyBy5Result = addAndMultiplyBy5(10, 5);
+
+console.log('addAndMultiplyBy5 Exercise:', addAndMultiplyBy5Result);
+
+// multiplyAndDivideBy5 Exercise
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1 * num2) / 5;
+}
+
+var multiplyAndDivideBy5Result = multiplyAndDivideBy5(35, 10);
+
+console.log('multiplyAndDivideBy5 Exercise:', multiplyAndDivideBy5Result);
+
+// subtractTwoNumbers Exercise
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+
+var subtractTwoNumbersResult = subtractTwoNumbers(22, 7);
+
+console.log('subtractTwoNumbers Exercise:', subtractTwoNumbersResult);
+
+// getCircleCircumference Exercise
+function getCircleCircumference(radius) {
+  return radius * 2 * Math.PI;
+}
+
+var getCircleCircumferenceResult = getCircleCircumference(5);
+
+console.log('getCircleCircumference Exercise:', getCircleCircumferenceResult);
+
+// getFullName Exercise
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+
+var getFullNameResult = getFullName('Dwight', 'Schrute');
+
+console.log('getFullName Exercise:', getFullNameResult);
+
+// cube Exercise
+function cube(number) {
+  return number * number * number;
+}
+
+var cubeResult = cube(5);
+
+console.log('cube Exercise:', cubeResult);
